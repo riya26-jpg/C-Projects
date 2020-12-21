8d70a18a6c483a5053d83f1593fcdeac07957209
Riya <62469802+riya26-jpg@users.noreply.github.com>
2020-12-21 15:03:47 +0530

    Add files via upload

---
+++ MousePos.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePos : MonoBehaviour
+{
+
+    public GameObject brush;
+    public float brushsize = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetMouseButton(1))
+        {
+            //cast a ray to the plane
+            var Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(Ray, out hit))
+            {
+                //instanciate a brush
+                var paint = Instantiate(brush, hit.point + Vector3.up * 0.1f, Quaternion.identity, transform);
+                paint.transform.localScale = Vector3.one * brushsize;
+            }
+
+        }
+
+    }
+}
---
+++ drag.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drag : MonoBehaviour
+{
+    private Vector3 screenPoint;
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 currentPos = Camera.main.ScreenToWorldPoint(currentScreenPoint);
+        transform.position = currentPos;
+    }
+}
